9b89024155f1edf698f1db079bb6cdfa3d013d2d
Luis Henrique <rickoliveira10@hotmail.com>
2020-03-26 18:13:42 -0300

    ExerciciosSemana5 - 100 primeiros números pares

---
+++ PrimeirosNumerosPares.java
@@ -0,0 +1,10 @@
+public class PrimeirosNumerosPares
+{
+   public static void main(String[] args)
+   {
+      int i;
+      for(i = 2; i =200; i++)
+         if(i%2 == 0)
+            System.out.println(i);
+   }
+}
